f75cfc084d1a2f160ffbe6029a86de11f86bb0c6
Marc Zhao <szhao17@gsb.columbia.edu>
2016-10-18 03:10:42 -0400

    Heroku part 1

--- gif.py
+++ gif.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from googlefinance import getQuotes
+import os
 import giphypop
 g = giphypop.Giphy()
 app = Flask(__name__)
@@ -18,4 +19,5 @@ def results():
     results = g.search(request.values.get('keywords'))
     return render_template('results.html', results=results)
 
-app.run(debug=False)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
